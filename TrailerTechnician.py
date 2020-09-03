c8b8d66ed509afd080c2d8dc3c945c2aeb5d90e9
jsaddiction <04reduramax@gmail.com>
2020-09-02 20:34:23 -0400

    fix downloads permissions

--- TrailerTechnician.py
+++ TrailerTechnician.py
@@ -20,6 +20,7 @@ def _download_trailer(directory):
     if not os.path.isdir(temp_dir):
         log.info('Creating temp directory. "{}"'.format(temp_dir))
         os.mkdir(temp_dir)
+        
     temp_trailer_path = os.path.join(temp_dir, directory.trailer_filename)
     
 
