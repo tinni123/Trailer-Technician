d89a3e4ef8a029480dacf1170d5a807bb09a4ae6
jsaddiction <04reduramax@gmail.com>
2020-09-03 18:14:47 -0400

    fix tmdb releasedate parsing

--- providers/tmdb.py
+++ providers/tmdb.py
@@ -65,7 +65,13 @@ def _handle_error(error):
 
 # Convert '2010-04-14' to '2010'
 def _release_date_to_year(release_date):
-    return str(datetime.strptime(release_date, '%Y-%m-%d').year)
+    if len(release_date) == 4:
+        return release_date
+    try:
+        return str(datetime.strptime(release_date, '%Y-%m-%d').year)
+    except ValueError as e:
+        log.warning('Release date from tmdb was incorrect format "{}"'.format(release_date))
+        return None
 
 # Main function to collect data about a movie
 def get_movie_info(tmdb_id=None, imdb_id=None, year=None, title=None):
